5a843c8c66fd985957592f46e56e43569796e37a
Nisha meena <nishameena2200@gmail.com>
2025-02-09 22:21:45 +0530

    first commit

---
+++ day_1.java
@@ -0,0 +1,9 @@
+//WAP to printb"Hello World".
+package code_challenges_100_days;
+
+import java.util.Scanner;
+public class day_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ day_3.java
@@ -0,0 +1,7 @@
+package code_challenges_100_days;
+
+public class day_3 {
+    public static void main(String[] args) {
+
+    }
+}
