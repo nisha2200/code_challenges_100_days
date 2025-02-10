3dff12b2038765cdbb4362911ba3ac986e238f6b
Nisha meena <nishameena2200@gmail.com>
2025-02-10 10:51:56 +0530

    5a843c8c66fd985957592f46e56e43569796e37a

--- day_5.java
+++ day_5.java
@@ -6,6 +6,6 @@ import java.util.Scanner;
 public class day_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int primenum = sc.nextInt();
+        int primeNum = sc.nextInt();
     }
 }
