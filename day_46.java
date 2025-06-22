867c1f398cfb4bf2009400d009881db1c59c7037
Nisha meena <nishameena2200@gmail.com>
2025-06-22 20:40:45 +0530

    another commit

--- day_46.java
+++ day_46.java
@@ -1,12 +1,15 @@
 package code_challenges_100_days;
+
 //Implement method overriding in an animal and dog class
+
 public class day_46 {
     static class Animal {
-        public void  sound(){
+        public void sound() {
             System.out.println("Animal makes a sound ");
         }
     }
-    static class Dog extends Animal{
+
+    static class Dog extends Animal {
         @Override
         public void sound() {
             System.out.println("Dog barks.");
---
+++ day_47.java
@@ -0,0 +1,51 @@
+package code_challenges_100_days;
+//Create an employee class with inheritance for manager and developer
+public class day_47 {
+    class  Employee{
+        String name ;
+        int id;
+        double salary;
+        public  Employee(String name , int id , double salary){
+            this.name = name;
+            this.id = id;
+            this.salary = salary;
+        }
+        public void displayInfo(){
+            System.out.println("Name: "+ name);
+            System.out.println("id: "+ id);
+            System.out.println("salary: "+ salary);
+        }
+    }
+    class Manager extends Employee{
+        String department;
+        public Manager(String name, int id, double salary, String department){
+            super(name , id , salary);
+            this.department = department;
+        }
+        public void displayManagerInfo(){
+            displayInfo();
+            System.out.println("Department: "+ department);
+        }
+    }
+    class  Developer extends  Employee{
+        String programmingLanguage;
+        public Developer(String name , int id, double salary, String programmingLanguage){
+            super(name , id , salary);
+            this.programmingLanguage = programmingLanguage;
+        }
+        public void displayDeveloperInfo(){
+            displayInfo();
+            System.out.println("Programming Lnaguage: "+ programmingLanguage);
+        }
+    }
+    public class Company{
+        public void main(String[] args) {
+            Manager manager = new Manager("alice", 11, 290000, "HR");
+            Developer developer = new Developer(" bob", 456, 234500, "java");
+            System.out.println("manager details: ");
+            manager.displayManagerInfo();
+            System.out.println("developer details: ");
+            developer.displayDeveloperInfo();
+        }
+    }
+}
