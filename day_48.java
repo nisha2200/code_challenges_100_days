d8ccb9fb407edd8a6113ffdd5ef41bda50c187b7
Nisha meena <nishameena2200@gmail.com>
2025-06-27 22:47:45 +0530

    another commit

---
+++ day_48.java
@@ -0,0 +1,8 @@
+package code_challenges_100_days;
+//Implement an abstract class Shape with subclass Circle and rectangle
+public class day_48 {
+
+    public static void main(String[] args) {
+
+    }
+}
