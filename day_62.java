39fda4fbc588ef1bfb053f2ccc725234b63c3fea
Nisha meena <nishameena2200@gmail.com>
2025-07-02 21:42:12 +0530

    again committing

--- day_62.java
+++ day_62.java
@@ -1,4 +1,29 @@
 package code_challenges_100_days;
 
+import java.util.LinkedList;
+
+//Demonstrate how to use Linkedlist to add and remove elements.
 public class day_62 {
+    public static void main(String[] args) {
+//        create  a linkedlist of strings
+        LinkedList<String> cities = new LinkedList<>();
+//        add elements to the linkedlist
+        cities.add("delhi");
+        cities.add("mumbai");
+        cities.add("bhopal");
+        cities.add("patna");
+        cities.add("bihar");
+//        display the linkedlist
+        System.out.println("Initial list: "+cities);
+//        add element at the beginning
+        cities.addFirst("Bangalore");
+//        add element at the end
+        cities.addLast("pune");
+        System.out.println("after adding elements: "+cities);
+//        remove first element
+        cities.removeFirst();
+        cities.removeLast();
+        cities.remove("mumbai");
+        System.out.println("After removing elements. "+cities);
+    }
 }
---
+++ day_63.java
@@ -0,0 +1,7 @@
+package code_challenges_100_days;
+//
+public class day_63 {
+    public static void main(String[] args) {
+
+    }
+}
